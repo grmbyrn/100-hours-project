e04a90c6cb7e82f60e01c684e888d185fae4c44f
grmbyrn <graemebyrne_89@hotmail.com>
2022-09-02 19:06:25 +0200

    added dependencies, server.js, set project up on localhost:3000, used express to get html

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log('Node')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3000, () => {
+    console.log('listening on 3000')
+})
