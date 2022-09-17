d4666111e4572a82f9beac2a4301d8c14e825c1c
grmbyrn <graemebyrne_89@hotmail.com>
2022-09-17 18:38:00 +0200

    added views folder for ejs, connected databse to server.js, created public folder

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 console.log('Node')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
+require('dotenv').config({path: './config/.env'})
 
-app.listen(3000, () => {
-    console.log('listening on 3000')
-})
+// Passport config
+// require('./config/passport')(passport)
+
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+    })
+  )
+  
+// Passport middleware
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+app.use(flash())
+
+// ADD ROUTES HERE
+// app.use('/', mainRoutes)
+// app.use('/todos', todoRoutes)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})  
