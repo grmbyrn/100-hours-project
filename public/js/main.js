997cf7614796db46a0890db504a836317c29048e
grmbyrn <graemebyrne_89@hotmail.com>
2023-01-16 12:50:27 +0100

    update js

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,5 @@
-// const hello = document.querySelector('#hello')
+const hello = document.querySelector('#hello')
 
-// hello.addEventListener('click', () => {
-//     console.log('hello')
-// })
-
-function buttonClicked(){
-    console.log("Button clicked");
-}
-
-window.onload=function(){
-  var btn = document.getElementById("hello");
-  btn.addEventListener("click", buttonClicked, true);
-}
+hello.addEventListener('click', () => {
+    console.log('hello')
+})
