b734fb7bc63cab58f0df9e0a4eae8e0cf5c6e498
grmbyrn <graemebyrne_89@hotmail.com>
2023-01-16 12:48:42 +0100

    update js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,14 @@
-const hello = document.querySelector('#hello')
+// const hello = document.querySelector('#hello')
 
-hello.addEventListener('click', () => {
-    console.log('hello')
-})
+// hello.addEventListener('click', () => {
+//     console.log('hello')
+// })
+
+function buttonClicked(){
+    console.log("Button clicked");
+}
+
+window.onload=function(){
+  var btn = document.getElementById("hello");
+  btn.addEventListener("click", buttonClicked, true);
+}
